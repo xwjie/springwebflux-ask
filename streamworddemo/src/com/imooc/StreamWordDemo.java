6b99944249f5018d5d92a2508c35d101abc934ef
xwjie <121509092@qq.com>
2018-04-26 18:57:34 +0800

    完善例子

--- streamworddemo/src/com/imooc/StreamWordDemo.java
+++ streamworddemo/src/com/imooc/StreamWordDemo.java
@@ -3,6 +3,10 @@ package com.imooc;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LongSummaryStatistics;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 使用stream统计文章单词数
@@ -32,6 +36,36 @@ public class StreamWordDemo {
           .sum();
 
       System.out.println("单词数:" + wordCount);
+    }
+
+    // 使用try-resource 关闭资源
+    try (BufferedReader reader = new BufferedReader(
+        new FileReader("webflux.txt"))) {
+
+      Map<String, Long> counts = reader.lines()
+          // trim前后空格(使用方法引用)
+          .map(String::trim)
+          // 过滤掉空串
+          .filter(s -> !s.isEmpty())
+          // 把空格隔开的转为数组
+          .map(s -> s.split(" "))
+          // 数组转成流
+          .map(array -> Stream.of(array))
+          // 拉平
+          .flatMap(stream -> stream)
+          // 分组
+          .collect(Collectors.groupingBy(s -> s, Collectors.counting()));
+
+      System.out.println("单词出现次数:" + counts);
+
+      // 统计信息
+      LongSummaryStatistics summaryStatistics = counts.entrySet().stream()
+          // 得到次数
+          .mapToLong(entry -> entry.getValue())
+          // 统计
+          .summaryStatistics();
+
+      System.out.println("统计信息:" + summaryStatistics);
     }
   }
 
